1acb9724ebae8e4025ed332cf0713cdda51a3894
Abhiraj Aditya <abhiaditya861@gmail.com>
2023-07-12 15:44:06 +0530

    flowerpredict

---
+++ model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+data = pd.read_csv('ML_Deployment_Test\Iris.csv')
+
+varietyMappings = {0: 'Setosa', 1: 'Versicolor', 2:'Virginica'}
+
+data = data.replace(['Setosa', 'Versicolor', 'Virginica'], [0, 1, 2])
+
+X = data.iloc[:, 0:-1]
+y = data.iloc[:, -1]
+
+logreg = LogisticRegression(max_iter=1000)
+logreg.fit(X, y)
+
+def classify(a, b, c, d):
+    arr = np.array([a, b, c, d])
+    arr = arr.astype(np.float64)
+    query = arr.reshape(1, -1)
+    prediction = varietyMappings[logreg.predict(query)[0]]
+    return prediction
+
+
---
+++ server.py
@@ -0,0 +1,27 @@
+import model
+from flask import Flask, request, render_template
+
+app = Flask(__name__,template_folder="templates")
+@app.route('/home')
+def home():
+    return render_template('home.html') # Render home.html
+
+@app.route('/classify',methods=['GET'])
+def classify_type():
+    try:
+        sepal_len = request.args.get('slen') # Get parameters for sepal length
+        sepal_wid = request.args.get('swid') # Get parameters for sepal width
+        petal_len = request.args.get('plen') # Get parameters for petal length
+        petal_wid = request.args.get('pwid') # Get parameters for petal width
+
+        # Get the output from the classification model
+        variety = model.classify(sepal_len, sepal_wid, petal_len, petal_wid)
+        # print("I am here", sepal_len, sepal_wid, petal_len, petal_wid)
+        # Render the output in new HTML page
+        return render_template('output.html', variety=variety)
+    except:
+        return 'Error Hello'
+    
+# Run the Flask server
+if(__name__=='__main__'):
+    app.run(debug=True)
